61e24f762c29740c4b23ffccf94d5ac725661f47
Athul H S <127603418+athulkadakkal@users.noreply.github.com>
2024-06-27 14:34:59 +0530

    Update arduino_uno_rx_nrf.ino

--- arduino_uno_rx_nrf.ino
+++ arduino_uno_rx_nrf.ino
@@ -21,16 +21,35 @@ int count =0;
 void setup() {
   Serial.begin(9600);
   radio.begin();
+  
+/////////////////////////////////////////////////////////////////////////////////////////
+  while (!Serial) { // some boards need to wait to ensure access to serial over USB
+  }
+  // initialize the transceiver on the SPI bus
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}  // hold in infinite loop
+  }
+  else Serial.println(F("radio hardware is  responding!!"));
+/////////////////////////////////////////////////////////////////////////////////////////
+  
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
 
 void loop() {
+  
   if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.print(text);
+    Serial.println(F("radio signal is available >> "));
+    //char text[32] = "";
+    //radio.read(&text, sizeof(text));
+    //Serial.print(text);
+
+    int data;
+    radio.read(&data, sizeof(data));
+    Serial.print(data);
+    
     Serial.print(" ");
     Serial.println(count);
     count++;
