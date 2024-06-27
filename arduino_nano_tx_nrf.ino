c4a496511f8055310b9f110223fbd22068eeb2e1
Athul H S <127603418+athulkadakkal@users.noreply.github.com>
2024-06-27 14:33:08 +0530

    Update arduino_nano_tx_nrf.ino

--- arduino_nano_tx_nrf.ino
+++ arduino_nano_tx_nrf.ino
@@ -16,8 +16,23 @@
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
-
+int data =0;
 void setup() {
+  
+////////////////////////////////////////////////////////////////////////////////////////
+  Serial.begin(9600);
+  while (!Serial) {
+    // some boards need to wait to ensure access to serial over USB
+  }
+
+  // initialize the transceiver on the SPI bus
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}  // hold in infinite loop
+  }
+  else Serial.println(F("radio hardware is  responding!!"));
+/////////////////////////////////////////////////////////////////////////////////////////
+  
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
@@ -25,8 +40,12 @@ void setup() {
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+  //const char text[] = "Hello World";
+  //radio.write(&text, sizeof(text));
+  radio.write(&data, sizeof(data));
+  Serial.println(data);
+  data++;
+  Serial.println(F("radio hardware is sending >> "));
   
   delay(1000);
 }
